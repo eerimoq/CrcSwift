2f34b2d5432770cb00559f844a7efe8c2909eda9
Erik Moqvist <erik.moqvist@gmail.com>
2025-07-19 13:48:54 +0200

    Optimize bit reversal.

--- Sources/CrcSwift/Helpers/DataHelper.swift
+++ Sources/CrcSwift/Helpers/DataHelper.swift
@@ -14,32 +14,29 @@ class DataHelper {
         
         return array
     }
-    
+
     static func reverseBits(_ data: UInt8) -> UInt8 {
-        var binaryString = String(data, radix: 2)
-        binaryString = String(repeating: "0", count: 8 - binaryString.count) + binaryString
-        
-        let revBinaryString = String(binaryString.reversed())
-        
-        return UInt8(revBinaryString, radix: 2) ?? 0
+        var value = data
+        value = (((value & 0xaa) >> 1) | ((value & 0x55) << 1))
+        value = (((value & 0xcc) >> 2) | ((value & 0x33) << 2))
+        return (((value & 0xf0) >> 4) | ((value & 0x0f) << 4))
     }
-    
+
     static func reverseBits(_ data: UInt16) -> UInt16 {
-        var binaryString = String(data, radix: 2)
-        binaryString = String(repeating: "0", count: 16 - binaryString.count) + binaryString
-        
-        let revBinaryString = String(binaryString.reversed())
-        
-        return UInt16(revBinaryString, radix: 2) ?? 0
+        var value = data
+        value = (((value & 0xaaaa) >> 1) | ((value & 0x5555) << 1))
+        value = (((value & 0xcccc) >> 2) | ((value & 0x3333) << 2))
+        value = (((value & 0xf0f0) >> 4) | ((value & 0x0f0f) << 4))
+        return (((value & 0xff00) >> 8) | ((value & 0x00ff) << 8))
     }
-    
+
     static func reverseBits(_ data: UInt32) -> UInt32 {
-        var binaryString = String(data, radix: 2)
-        binaryString = String(repeating: "0", count: 32 - binaryString.count) + binaryString
-        
-        let revBinaryString = String(binaryString.reversed())
-        
-        return UInt32(revBinaryString, radix: 2) ?? 0
+        var value = data
+        value = (((value & 0xaaaaaaaa) >> 1) | ((value & 0x55555555) << 1))
+        value = (((value & 0xcccccccc) >> 2) | ((value & 0x33333333) << 2))
+        value = (((value & 0xf0f0f0f0) >> 4) | ((value & 0x0f0f0f0f) << 4))
+        value = (((value & 0xff00ff00) >> 8) | ((value & 0x00ff00ff) << 8))
+        return ((value >> 16) | (value << 16))
     }
     
 }
